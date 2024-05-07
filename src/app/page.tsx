4d08e08c0c913a64dca26073dd92e69d4c1f5ff9
myaji35 <32953110+myaji35@users.noreply.github.com>
2024-05-07 18:48:15 +0900

    button componunt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <h1>Home....way not1111222221?</h1>
+    <div>
+      <button>Hello from the index page111</button>
+    </div>
   );
 }
